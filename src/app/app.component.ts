f30acd90113961a4639d09c76ef35a40ac0ebe6f
luismtapiab <lmtapia1024@gmail.com>
2025-05-01 15:42:21 -0400

    showing some products

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgxSonnerToaster } from 'ngx-sonner';
-
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-})
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgxSonnerToaster],
+  imports: [RouterOutlet, NgxSonnerToaster, HeaderComponent],
   template: `
     <ngx-sonner-toaster 
       position="top-center" richColors
@@ -22,7 +15,7 @@ import { NgxSonnerToaster } from 'ngx-sonner';
       duration="5000"
       offset="100px" 
     />
-    <app-header/>
+    <app-header />
     <router-outlet />
   `,
   styles: [],
