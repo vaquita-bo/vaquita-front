1776b9e74daded42968bd0053e9cf64554de67bc
luismtapiab <lmtapia1024@gmail.com>
2025-05-13 22:26:42 +0000

    [UPDATE] Payment and paid dialogs done

--- src/app/client/components/paid-dialog/paid-dialog.component.ts
+++ src/app/client/components/paid-dialog/paid-dialog.component.ts
@@ -1,15 +1,23 @@
+import { DatePipe, formatDate } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-paid-dialog',
-  imports: [],
+  imports: [MatIconModule, DatePipe],
   templateUrl: './paid-dialog.component.html',
   styleUrl: './paid-dialog.component.css'
 })
 export class PaidDialogComponent {
   data = inject(MAT_DIALOG_DATA)
+  
+  today= new Date();
+  jstoday = '';
+  
   constructor(public dialogRef: MatDialogRef<PaidDialogComponent>){
+    this.jstoday = formatDate(this.today, 'dd-MM-yyyy hh:mm:ss a', 'en-US', '+0530');
   }
 
   closeSelf(){
